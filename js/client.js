7eb6280d1e559ddfca96a6d751f3ee4e36781e9e
val kharitonashvili <valeri.kharitonashvili1@gmail.com>
2016-02-15 16:52:58 +0400

    about me

--- js/client.js
+++ js/client.js
@@ -1,13 +1,40 @@
 $(document).ready(function () {
 
+    //name type
     $(".typed").typed({
         strings: ["I am <span>val</span> kharitonashvili"],
         typeSpeed: 100,
         showCursor: false
     });
 
+
     $('.menu').on('click', function () {
+        $(this).toggleClass('active');
         $(this).siblings('ul').toggleClass('active');
+        $('.fa-bars').toggleClass('hide');
+        $('.fa-times').toggleClass('active');
+        $('.main-section li, .about-me, .main-info').removeClass('active');
+    });
+
+    //menu active
+
+    $('.main-section li').on('click', function () {
+        $('.main-section li').removeClass('active');
+        $(this).addClass('active');
+    });
+
+
+    //about
+    $('.about-nav').on('click', function () {
+        $('.main-info, .border, .about-me').toggleClass('active');
+
+        //about me type
+        $(".about-me-type").typed({
+            strings: ["about <span>me</span> "],
+            typeSpeed: 100,
+            showCursor: false
+        });
+
     });
 
 });
